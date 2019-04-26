a84ab17fb20ed3d06ff05bbb9f723d1068287115
MatveevDM <cn14@mail.ru>
2019-04-26 17:28:44 +0300

    Создание вспомогательных методов для контактов

--- test_add_contact.py
+++ test_add_contact.py
@@ -4,20 +4,29 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
-class TestAddContact(unittest.TestCase):
+class test_add_contact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_contact(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd):
+        # open add new
         wd.find_element_by_link_text("add new").click()
+        # init contact creation
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("123")
         wd.find_element_by_name("middlename").clear()
@@ -32,10 +41,19 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("company").send_keys("111")
         wd.find_element_by_name("address").clear()
         wd.find_element_by_name("address").send_keys("222")
+        # submit contact creation
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def login(self, wd):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
