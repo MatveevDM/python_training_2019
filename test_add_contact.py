a256b8a240ae18f42eec45c7874c62d0ab13d18d
MatveevDM <cn14@mail.ru>
2019-04-26 17:53:07 +0300

    Создал отдельный класс для параметров контакта

--- test_add_contact.py
+++ test_add_contact.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group_contact import Group_contact
 
 class test_add_contact(unittest.TestCase):
     def setUp(self):
@@ -13,7 +14,7 @@ class test_add_contact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, firstname="123", middlename="345", lastname="567", nickname="789", title="890", company="111", address="222")
+        self.create_contact(wd, Group_contact(firstname="123", middlename="345", lastname="567", nickname="789", title="890", company="111", address="222"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -21,7 +22,7 @@ class test_add_contact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, firstname="", middlename="", lastname="", nickname="", title="", company="", address="")
+        self.create_contact(wd, Group_contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address=""))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -31,24 +32,24 @@ class test_add_contact(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_contact(self, wd, firstname, middlename, lastname, nickname, title, company, address):
+    def create_contact(self, wd, group_contact):
         # open add new
         wd.find_element_by_link_text("add new").click()
         # init contact creation
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(group_contact.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(group_contact.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(group_contact.lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(group_contact.nickname)
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(group_contact.title)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(group_contact.company)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group_contact.address)
         # submit contact creation
         wd.find_element_by_name("submit").click()
 
