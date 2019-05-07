3f71ab90b5658acb3c2c6ca0f4725dc477bea16f
MatveevDM <cn14@mail.ru>
2019-05-07 11:55:46 +0300

    Добавил удаление первого контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -9,25 +9,35 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
 
-    def create(self, group_contact):
+    def create(self, contact):
         wd = self.app.wd
         # open add new
         wd.find_element_by_link_text("add new").click()
         # init contact creation
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group_contact.firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group_contact.middlename)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group_contact.lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group_contact.nickname)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(group_contact.title)
+        wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group_contact.company)
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group_contact.address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         # submit contact creation
         wd.find_element_by_name("submit").click()
         self.return_to_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        # accept alert
+        wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("Last name").is_displayed()
