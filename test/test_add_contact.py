a259e0dfeb162db2698a3c0947eb69c59a6e7b12
MatveevDM <cn14@mail.ru>
2019-05-08 15:28:43 +0300

    Повышение "интеллектуальности" фикстуры
    Валидация

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="123", middlename="345", lastname="567", nickname="789", title="890", company="111", address="222"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address=""))
-    app.session.logout()
