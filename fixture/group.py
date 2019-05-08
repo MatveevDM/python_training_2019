c9939bad5daf1852df09a1be31a8b01a8019e500
MatveevDM <cn14@mail.ru>
2019-05-08 14:27:06 +0300

    Оптимизировал ввод данных в формы группы
    if -> then -> else

--- fixture/group.py
+++ fixture/group.py
@@ -15,43 +15,48 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        self.enter_info_group(group, wd)
+        self.enter_info_group(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def enter_info_group(self, group, wd):
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+    def enter_info_group(self, group):
+        self.change_field("group_name", group.name)
+        self.change_field("group_header", group.header)
+        self.change_field("group_footer", group.footer)
+
+    def change_field(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def update_first_contact(self, group):
+    def update_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # submit updation
+        self.select_first_group()
+        # open modification form
         wd.find_element_by_name("edit").click()
-        self.enter_info_group(group, wd)
+        # fill group form
+        self.enter_info_group(new_group_data)
+        #submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
